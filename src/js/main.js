13ff10cbe38eb7ccb9c2641ac969611688ef2bce
Camila Katayama <camilakatayamap@gmail.com>
2024-05-31 18:37:47 -0300

    added adopt modal with style in css

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,22 @@ function back() {
 function viewSuccess() {
     window.location.href = "./src/pages/success.html"
 }
+
+let adoptModal = document.getElementById('apply-to-adopt');
+let donateModal = document.getElementById('donation');
+
+function handlerAdoption() {
+    adoptModal.showModal();
+}
+
+function handlerDonation() {
+    donateModal.showModal();
+}
+
+function closeAdoptModal() {
+    adoptModal.close();
+}
+
+function cloaseDonateModal() {
+    donateModal.close();
+}
